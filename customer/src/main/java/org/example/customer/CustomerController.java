35a99b5248b1d70cd1437936ffb2fa6af9e6e4fb
Kostas Aleiferis <kaleiferis@adacom.com>
2024-12-19 13:50:09 +0200

    postgress and pgadmin on Docker and spring jpa

--- customer/src/main/java/org/example/customer/CustomerController.java
+++ customer/src/main/java/org/example/customer/CustomerController.java
@@ -1,6 +1,5 @@
 package org.example.customer;
 
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequestMapping("api/v1/customers")
 public record CustomerController(CustomerService customerService) {
--- customer/src/main/java/org/example/customer/CustomerService.java
+++ customer/src/main/java/org/example/customer/CustomerService.java
@@ -3,8 +3,10 @@ package org.example.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = new Customer(request.firstName(), request.lastName(), request.email());
+
+        customerRepository.save(customer);
     }
 }
